f069fbc0477eec708a9a190d0936433a04efbad9
SH Kim <82145134+shkim-123@users.noreply.github.com>
2023-05-24 22:02:08 +0900

    서블릿 예외 처리 - 필터

--- src/main/java/hello/exception/filter/LogFilter.java
+++ src/main/java/hello/exception/filter/LogFilter.java
@@ -21,6 +21,7 @@ public class LogFilter implements Filter {
             log.info("REQUEST [{}][{}][{}]", uuid, request.getDispatcherType(), requestURI);
             chain.doFilter(request, response);
         } catch (Exception e) {
+            log.info("EXCEPTION {}", e.getMessage());
             throw e;
         } finally {
             log.info("RESPONSE [{}][{}][{}]", uuid, request.getDispatcherType(), requestURI);
